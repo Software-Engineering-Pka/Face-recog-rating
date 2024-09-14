fddfdcceb368d4580e91a8281e4756b4fd62f1d4
“Tan <dinhthai0312@gmail.com>
2024-09-14 20:38:30 +0700

    fix

--- face_rating/face_rating/settings.py
+++ face_rating/face_rating/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "face_rating",
         "USER": "root",
-        "PASSWORD": "quyen2902@st#HH",
+        "PASSWORD": "dinhthai2004",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
